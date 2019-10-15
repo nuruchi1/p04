bac8427ee04d8a19cf9dd12396fca95ec0e928af
Nathaly Uruchima <nuruchi1@binghamton.edu>
2019-10-14 23:20:46 -0400

    add initial project files

---
+++ p04/p04Tests/p04Tests.swift
@@ -0,0 +1,34 @@
+//
+//  p04Tests.swift
+//  p04Tests
+//
+//  Created by Nathaly Uruchima on 10/14/19.
+//  Copyright © 2019 Nathaly Uruchima. All rights reserved.
+//
+
+import XCTest
+@testable import p04
+
+class p04Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
