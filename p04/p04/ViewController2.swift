7a6fb277ea4de2f715a816af6e7a2d1a7cb01217
Nathaly Uruchima <nuruchi1@binghamton.edu>
2019-10-20 23:47:40 -0400

    add second view controller and buttons

---
+++ p04/p04/ViewController2.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController2.swift
+//  p04
+//
+//  Created by Nathaly Uruchima on 10/20/19.
+//  Copyright © 2019 Nathaly Uruchima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
