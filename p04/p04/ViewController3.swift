0f9e9c0d66ef631659c132dd6f78b938c16f17d4
Nathaly Uruchima <nuruchi1@binghamton.edu>
2019-10-24 23:37:49 -0400

    add code for game screen

--- p04/p04/ViewController3.swift
+++ p04/p04/ViewController3.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import SpriteKit
+import GameplayKit
+var userScore = 0;
 
-class ViewController3: UIViewController {
+class ViewController3: UIViewController, SKPhysicsContactDelegate {
 
-    override func viewDidLoad() {
+   override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
+  /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
