f0b5f499394a61904dd4b199967b03e232018def
Nathaly Uruchima <nuruchi1@binghamton.edu>
2019-11-04 15:23:47 -0500

    add to README.md file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-
+For this project I have created a shooter like game using characters from AnimalCrossing. There is a home screen, with a play, how to play, and exit button.  
 
 October 14th - created git repository and added initial project files
 
@@ -10,4 +10,11 @@ October 21st - added more buttons to main screen and third view controller
 October 23rd - added buttons and a final view controller. Made sure all buttons work as of now. added ViewController files that were missing. 
 
 October 24th - found sprites for game and added code for the main game. 
+
+October 25th - added code for main menu screen
+
+October 26th - edited and added code for main menu
+
+October 28th - added code for game implementation
+
 # p04
--- p04/p04/ViewController2.swift
+++ p04/p04/ViewController2.swift
@@ -6,22 +6,56 @@
 //  Copyright © 2019 Nathaly Uruchima. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import SpriteKit
-import GameKit
-class ViewController2: SKScene, SKPhysicsContactDelegate {
 
-  
-    
+class ViewController2: SKScene {
 
+    override func didMove(to view: SKView)
+    {
+        let background = SKSpriteNode(imageNamed: "mainscreenBackground")
+            background.position = CGPoint(x: self.size.width/2, y:self.size.height/2)
+            background.zPosition = 0
+            self.addChild(background)
+               
+               
+        let titleLabel = SKLabelNode(fontNamed: "FinkHeavy")
+            titleLabel.text = "How to Play:"
+            titleLabel.fontSize = 100
+            titleLabel.fontColor = SKColor.black
+            titleLabel.zRotation = 0
+            titleLabel.position = CGPoint(x: 50, y: 100)
+            titleLabel.zPosition = 2
+            self.addChild(titleLabel)
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let infoLabel = SKLabelNode(fontNamed: "FinkHeavy")
+            infoLabel.text = "Isabelle has been taking care of the town ever since the Mayor dissapeared."
+            infoLabel.fontSize = 40
+            infoLabel.fontColor = SKColor.black
+            infoLabel.zRotation = 0
+            infoLabel.position = CGPoint(x: 0, y: 90)
+            infoLabel.zPosition = 1
+            self.addChild(infoLabel)
+        
+         let infoLabel2 = SKLabelNode(fontNamed: "FinkHeavy")
+             infoLabel2.text = "Recently the Town has been attacked by familiar faces and the villagers have abandoned the Town out of fear!"
+             infoLabel2.fontSize = 40
+             infoLabel2.fontColor = SKColor.black
+             infoLabel2.zRotation = 0
+             infoLabel2.position = CGPoint(x: 0, y: 70)
+             infoLabel2.zPosition = 1
+             self.addChild(infoLabel2)
+        
+        let infoLabel3 = SKLabelNode(fontNamed: "FinkHeavy")
+            infoLabel3.text = "Help Isabelle defeat the attackers and make the Town safe again!"
+            infoLabel3.fontSize = 40
+            infoLabel3.fontColor = SKColor.black
+            infoLabel3.zRotation = 0
+            infoLabel3.position = CGPoint(x: 0, y: 50)
+            infoLabel3.zPosition = 1
+            self.addChild(infoLabel3)
+*/
+        
     }
-    */
-
+   
 }
